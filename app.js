d403edd2355b725d85529c93131f3f2702b704ed
Samuel Caetité <sscaetite@gmail.com>
2019-03-01 14:42:29 -0300

    Change http GET method to POST

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
     res.send("Você não deveria estar aqui");
 })
 
-app.get('/inscricao/desafio-de-impacto/2019_2', (req, res) => {
+app.post('/inscricao/desafio-de-impacto/2019_2', (req, res) => {
         
     const htmlObject = new JSDOM(req.body);
 
