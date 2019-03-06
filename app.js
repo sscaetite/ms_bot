0b43c4eb944072d69c1748534c1e38e7ee65390e
Samuel Caetité <sscaetite@gmail.com>
2019-03-06 08:37:22 -0300

    QQQQQQQQQQQQQQQQ

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
 
 app.post('/incricaowebdev', (req, res) => {
     const htmlObject = new JSDOM(req.body);
-    const conteudo = htmlObject.window.document.querySelector('#estilo');
+    let conteudo = htmlObject.window.document.querySelector('#estilo');
     if(conteudo) conteudo = conteudo.textContent;
     else conteudo = "Ruim"
     res.send(conteudo);
