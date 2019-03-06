81599f72837dfa2809732efc92adc25969b95f92
Samuel Caetité <sscaetite@gmail.com>
2019-03-06 04:34:58 -0300

    Code refatored!

--- app.js
+++ app.js
@@ -4,89 +4,84 @@ const bodyParser = require('body-parser');
 const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 
-const port = 80;
-
 app.use(bodyParser.text({ type: 'text/html' }));
 app.use(bodyParser.urlencoded({ extended: false }));
+
+//Retorna o conteúdo de texto presente numa tag com determinada ID do DOM
+const returnItemByID = (htmlId, objectHtml) => {
+    const selector = objectHtml.window.document.querySelector(htmlId);
+    if(!selector) return "-";
+    return selector.textContent;
+};
+
+//Transforma o texto de uma req.body em string
+const transformReqBodyIntoString = (bodyRequest) => {
+    let textString = '';
+    for (let i = 0; bodyRequest[i] != "*"; i++) {
+        if(bodyRequest[i] != "\"" && bodyRequest[i] !== "\n" && bodyRequest[i] !== "\r") textString = textString + bodyRequest[i];
+        if(bodyRequest[i] == "\"") textString = textString + "##";
+    }
+    return textString;
+}
+
+//Lista todas as IDs de uma req.body
+const listAllIds = (textoRequest) => {
+    let allDivIds = [];
+    let initialPos = 0;
+    let finalPos = 0;
+    while(initialPos != -1 && finalPos != -1) {
+
+        initialPos = textoRequest.indexOf("id=#", finalPos);
+        finalPos = textoRequest.indexOf("##", initialPos + 4);
+        allDivIds[allDivIds.length] = textoRequest.substring(initialPos + 4, finalPos)
+        initialPos = textoRequest.indexOf("id=#", finalPos);
+    }
+
+    return allDivIds;
+
+}
+
+//Converte o nome de uma string para camelcase
+const parseIdNameToCamelCase = (nomeId) => {
+    
+    if(nomeId) nomeId = nomeId.toString();
+    else nomeId = "";
+
+    for(let i = 0; i < nomeId.length; i++) {
+        if(nomeId[i] == "-") {
+            nomeId = nomeId.substr(0,i) + nomeId.charAt(i+1).toUpperCase() + nomeId.substr(i+2);
+            i = i - 1;
+        }
+    };
+    nomeId = nomeId.charAt(0).toUpperCase() + nomeId.substring(1, nomeId.length);
+    return nomeId;
+};
+
+//Converte um documento HTML em um objeto JSON
+const returnJSONFromHtml = (stringRequest, htmlObject) => {
+
+    const formatedHtml = {};
+
+    const idsDocument = listAllIds(stringRequest);      
+
+    for(let i = 0; i  < idsDocument.length; i++) {
+        formatedHtml[parseIdNameToCamelCase(idsDocument[i].substring(1, idsDocument[i].length))] = returnItemByID(idsDocument[i], htmlObject);
+    }
+    
+    return formatedHtml;
+};
 
 app.get('/', (req, res) => {
     res.send("Você não deveria estar aqui");
 })
 
-//Rota para o Léo hétero entrar
-app.get('/leosoares', (req,res) => {
-    res.send("Leo, vc é um amor! E gado demais");
-})
-
 app.post('/inscricao', (req, res) => {    
 
-    const htmlObject = new JSDOM(req.body);
-
-    //Retorna o conteúdo de texto presente numa tag com determinada ID do DOM
-    const returnItemByID = (htmlId) => {
-        const selector = htmlObject.window.document.querySelector(htmlId);
-        if(!selector) return "-";
-        return selector.textContent;
-    };
-
-    //Transforma o texto de uma req.body em string
-    const transformReqBodyIntoString = () => {
-        let textString = '';
-        for (let i = 0; req.body[i] != "*"; i++) {
-            if(req.body[i] != "\"" && req.body[i] !== "\n" && req.body[i] !== "\r") textString = textString + req.body[i];
-            if(req.body[i] == "\"") textString = textString + "##";
-        }
-        return textString;
-    }
-
-    const textoReq = transformReqBodyIntoString();
-
-    const listAllIds = () => {
-        let allDivIds = [];
-        let initialPos = 0;
-        let finalPos = 0;
-        while(initialPos != -1 && finalPos != -1) {
-
-            initialPos = textoReq.indexOf("id=#", finalPos);
-            finalPos = textoReq.indexOf("##", initialPos + 4);
-            allDivIds[allDivIds.length] = textoReq.substring(initialPos + 4, finalPos)
-            initialPos = textoReq.indexOf("id=#", finalPos);
-        }
-
-        return allDivIds;
-
-    }
-
-    const parseIdNameToCamelCase = (nomeId) => {
+    const htmlJsdomObject = new JSDOM(req.body);
     
-        if(nomeId) nomeId = nomeId.toString();
-        else nomeId = "";
-    
-        for(let i = 0; i < nomeId.length; i++) {
-            if(nomeId[i] == "-") {
-                nomeId = nomeId.substr(0,i) + nomeId.charAt(i+1).toUpperCase() + nomeId.substr(i+2);
-                i = i - 1;
-            }
-        };
-    
-        return nomeId;
-    };
+    const textoReq = transformReqBodyIntoString(req.body);
 
-    //Converte um documento HTML em um objeto JSON
-    const returnJSONFromHtml = () => {
-
-        const formatedHtml = {};
-
-        const idsDocument = listAllIds();      
-
-        for(let i = 0; i  < idsDocument.length; i++) {
-            formatedHtml[parseIdNameToCamelCase(idsDocument[i].substring(1, idsDocument[i].length))] = returnItemByID(idsDocument[i]);
-        }
-        
-        return formatedHtml;
-    };
-
-    res.send(listAllIds());
+    res.send(returnJSONFromHtml(textoReq, htmlJsdomObject));
 
 });
 
