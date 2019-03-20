5a82fbee9d041f717c528f054a886161140a5efb
Samuel Caetité <sscaetite@gmail.com>
2019-03-20 17:36:08 -0300

    Agora sim, vai

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 const returnItemByID = (htmlId, objectHtml) => {
     let selectorOfId = objectHtml.window.document.querySelector(htmlId);
     if(!selectorOfId) return null;
-    if(selectorOfId.textContent == "") return "-";
+    if(selectorOfId.textContent == "") return null;
     return selectorOfId.textContent;
 };
 
